d06b732202f7c64ecb53f5c6f14de6c40ff205ce
abhishek <abhisheklwagun1@gmail.com>
2021-06-02 21:49:25 +0545

    fix the error

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 
 const Discord = require("discord.js");
 const { prefix, token } = require("./config.json");
-const keepAlive = require("./server")
+const ytdl = require("ytdl-core");
+
+const keepAlive = require("./server");
 
 const client = new Discord.Client();
 
 const queue = new Map();
+
+
 
 client.once("ready", () => {
     console.log("Ready!");
